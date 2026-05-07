8b6bad47c9e9a7c6d8a84064e2d5834cf5eabfe9
WOC-008 <emmanweke211@gmail.com>
2026-05-07 20:40:24 +0100

    Update radio1.ino

--- radio1.ino
+++ radio1.ino
@@ -5,14 +5,14 @@
 #include <LiquidCrystal_I2C.h>
 
 // Pin definitions
-#define BT_VOLUP        0
-#define BT_VOLDOWN      1
-#define PIN_MODE_BTN    4
-#define PIN_PWR_SIM     6
-#define PIN_BT_PWR      7
+#define BT_VOLUP 0
+#define BT_VOLDOWN 1
+#define PIN_MODE_BTN 4
+#define PIN_PWR_SIM 6
+#define PIN_BT_PWR 7
 
 #define LONG_PRESS_MS 3000
-#define RDS_MAX       150
+#define RDS_MAX 150
 
 const int PIN_ENC_FREQ_A = 2;
 const int PIN_ENC_FREQ_B = 3;
@@ -21,79 +21,80 @@ volatile int encoderValue = 0;
 volatile int lastEncoded = 0;
 
 
-const float FM_MIN =  64.0;
-const float FM_MAX =  108.0;
+const float FM_MIN = 64.0;
+const float FM_MAX = 108.0;
 const float FM_STEP = 0.1;
 
-const int AM_MIN =  520;
-const int AM_MAX =  1710;
+const int AM_MIN = 520;
+const int AM_MAX = 1710;
 const int AM_STEP = 10;
 
-const int SW_MIN =  2300;
-const int SW_MAX =  26100;
+const int SW_MIN = 2300;
+const int SW_MAX = 26100;
 const int SW_STEP = 5;
 
-const int LW_MIN =  144;
-const int LW_MAX =  285;
+const int LW_MIN = 144;
+const int LW_MAX = 285;
 const int LW_STEP = 10;
 
 const int volmin = 0;
 const int volmax = 63;
 const int volchange = 1;
 
-const int stepDelay =    300;
-const int holdDelay =    500;
+const int stepDelay = 300;
+const int holdDelay = 500;
 
 //change address as requid
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 SI4735 radio;
 
-enum Mode { MODE_FM,
-            MODE_AM,
-            MODE_SW,
-            MODE_LW,
-            MODE_BT 
-            };
+enum Mode {
+  MODE_FM,
+  MODE_AM,
+  MODE_SW,
+  MODE_LW,
+  MODE_BT
+};
 
 Mode currentMode = MODE_FM;
 
 float freqFM = 95.3;
-int freqAM =   1000;
-int freqSW =   16000;
-int freqLW =   150;
+int freqAM = 1000;
+int freqSW = 16000;
+int freqLW = 150;
 
-int volStep =     40;
+int volStep = 40;
 int lastVolStep = 0;
-int dotCount =    0;
-int scrollPos =   0;  // scrolling position
-int btn =         0;
-int lastbtn =     0;
+int dotCount = 0;
+int scrollPos = 0;  // scrolling position
+int btn = 0;
+int lastbtn = 0;
 
-unsigned long startupStart =     0;
-unsigned long lastDotTime =      0;
-unsigned long lastfreqCheck =    0;
-unsigned long lastModeCheck =    0;
+unsigned long startupStart = 0;
+unsigned long lastDotTime = 0;
+unsigned long lastfreqCheck = 0;
+unsigned long lastModeCheck = 0;
 unsigned long modeBtnPressTime = 0;
-unsigned long longpressTime =    0;
-unsigned long pressStartTime =   0;
-unsigned long lastlcdupdate =    0;
-unsigned long lastupdatee =      0;
-unsigned long actionint =        0;
+unsigned long longpressTime = 0;
+unsigned long pressStartTime = 0;
+unsigned long lastlcdupdate = 0;
+unsigned long lastupdatee = 0;
+unsigned long actionint = 0;
 unsigned long LAST_RDS_INTERVA = 0;
-unsigned long wait =             0;
-unsigned long volumeExitTime =  0;
+unsigned long wait = 0;
+unsigned long volumeExitTime = 0;
 
 const unsigned long muteCheckInterval = 100;
 const unsigned long ModeCheckInterval = 75;
 const unsigned long norm = 6000;
 const unsigned long RDS_INTERVAL = 470;
 
-bool radioIsOn = true;  
+bool radioIsOn = true;
 bool btModeDisplayed = false;
 bool run = true;
 bool first = true;
 bool isHolding = false;
-bool Muted    = false;
+bool Muted = false;
 bool btMode = false;
 bool startupInProgress = true;
 bool modeBtnHeld = false;
@@ -101,10 +102,10 @@ bool volaction = false;
 bool clrtogo = false;
 bool VOLUME = false;
 bool firstpress = false;
-bool second_press= false;
+bool second_press = false;
 bool THIRDPRESS = false;
-bool volumeExitPending =  false;
-bool lastVolumeState =     false;
+bool volumeExitPending = false;
+bool lastVolumeState = false;
 
 char rdsText[RDS_MAX];  // buffer for full RDS text
 
@@ -113,12 +114,12 @@ int lastValue = 0;
 void setup() {
   pinMode(PIN_ENC_FREQ_A, INPUT);
   pinMode(PIN_ENC_FREQ_B, INPUT);
-  pinMode(A3,INPUT);
+  pinMode(A3, INPUT);
   pinMode(PIN_MODE_BTN, INPUT_PULLUP);
   pinMode(PIN_BT_PWR, OUTPUT);
   pinMode(PIN_PWR_SIM, OUTPUT);
-  pinMode(BT_VOLUP, OUTPUT); 
-  pinMode(BT_VOLDOWN, OUTPUT); 
+  pinMode(BT_VOLUP, OUTPUT);
+  pinMode(BT_VOLDOWN, OUTPUT);
   digitalWrite(PIN_BT_PWR, HIGH);
   digitalWrite(PIN_PWR_SIM, LOW);
   digitalWrite(BT_VOLUP, LOW);
@@ -210,19 +211,19 @@ void loop() {
   }
   if (volumeExitPending) {
     if (millis() - volumeExitTime >= 2000) {
-        volumeExitPending = false;
-        if (!VOLUME) {
-            updateradioDisplay();
-        }
+      volumeExitPending = false;
+      if (!VOLUME) {
+        updateradioDisplay();
+      }
     }
-}
-  handleModeButton(); 
+  }
 
-   if (millis() - LAST_RDS_INTERVA >= RDS_INTERVAL
-   ){
-     LAST_RDS_INTERVA = millis();
-  updateRDS();
-   }
+  handleModeButton();
+
+  if (millis() - LAST_RDS_INTERVA >= RDS_INTERVAL) {
+    LAST_RDS_INTERVA = millis();
+    updateRDS();
+  }
 
   int valueCopy;
   int diff;
@@ -232,17 +233,17 @@ void loop() {
   valueCopy = encoderValue;
   interrupts();
 
-  diff =(valueCopy - lastValue)/4;
+  diff = (valueCopy - lastValue) / 4;
 
   if (diff != 0) {
-    lastValue +=diff*4;
+    lastValue += diff * 4;
 
-  if (VOLUME && !Muted){
+    if (VOLUME && !Muted) {
       handlevolumeControl(diff);
-  } else if (!Muted || Muted) {
-    handleFrequencyControl(diff);
+    } else if (!Muted || Muted) {
+      handleFrequencyControl(diff);
+    }
   }
-  } 
 
   if (btMode) {
     if (radioIsOn) {
@@ -283,8 +284,9 @@ void loop() {
     int voldisp = (volStep * 1);
     lcd.setCursor(0, 1);
     lcd.print("V:");
-    if (voldisp<10){
-    lcd.print(0);}
+    if (voldisp < 10) {
+      lcd.print(0);
+    }
     lcd.print(voldisp);
     radioIsOn = true;
   }
@@ -316,7 +318,7 @@ void animateStartup() {
   if (elapsed > 4000) {
     startupInProgress = false;
     lcd.clear();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("RADIO MODE");
     radio.setVolume(5);
     delay(400);
@@ -341,27 +343,25 @@ void handleModeButton() {
   } else if (lastState == LOW && !VOLUME && state == LOW && (!modeBtnHeld && (millis() - modeBtnPressTime > LONG_PRESS_MS))) {
     modeBtnHeld = true;
     btMode = !btMode;
-  } else if (lastState == LOW  && state == HIGH && !firstpress && !modeBtnHeld && (millis()-modeBtnPressTime < 1050)) {
+  } else if (lastState == LOW && state == HIGH && !firstpress && !modeBtnHeld && (millis() - modeBtnPressTime < 1200)) {
     firstpress = true;
-  } else if(lastState == LOW && state == HIGH && firstpress && !second_press && !modeBtnHeld && (millis()-modeBtnPressTime < 1050)){
-    second_press= true;
-  } else if(lastState == LOW && state ==HIGH && firstpress && !THIRDPRESS && second_press && !modeBtnHeld && (millis()-modeBtnPressTime < 1050)){
+  } else if (lastState == LOW && state == HIGH && firstpress && !second_press && !modeBtnHeld && (millis() - modeBtnPressTime < 1200)) {
+    second_press = true;
+  } else if (lastState == LOW && state == HIGH && firstpress && !THIRDPRESS && second_press && !modeBtnHeld && (millis() - modeBtnPressTime < 1200)) {
     THIRDPRESS = true;
-  } 
-  
-  if (THIRDPRESS == true && !VOLUME && !modeBtnHeld && ( millis() - modeBtnPressTime < 1100)){
-    second_press= false;
+  }
+
+  if (THIRDPRESS == true && !VOLUME && !modeBtnHeld && (millis() - modeBtnPressTime < 1250)) {
+    second_press = false;
     firstpress = false;
-    THIRDPRESS= false;
-    if (!btMode){
+    THIRDPRESS = false;
+    if (!btMode) {
       currentMode = (Mode)((currentMode + 3) % 4);
       switch (currentMode) {
         case MODE_FM:
           radio.setFM();
           radio.setFrequency((uint16_t)(freqFM * 100));
           updateradioDisplay();
-          lcd.setCursor(5, 1);
-          lcd.print("           ");
           break;
         case MODE_AM:
           radio.setAM();
@@ -384,10 +384,10 @@ void handleModeButton() {
     }
   }
 
-  if (!modeBtnHeld && second_press == true && !VOLUME && ( millis() - modeBtnPressTime >1100)) {
-    second_press= false;
+  if (!modeBtnHeld && second_press == true && !VOLUME && (millis() - modeBtnPressTime > 1250)) {
+    second_press = false;
     firstpress = false;
-    if (!btMode){
+    if (!btMode) {
       currentMode = (Mode)((currentMode + 1) % 4);
       switch (currentMode) {
         case MODE_FM:
@@ -416,68 +416,68 @@ void handleModeButton() {
     }
   }
 
-   if (firstpress == true && ( millis() - modeBtnPressTime >= 1100) ){
+  if (firstpress == true && (millis() - modeBtnPressTime >= 1250)) {
     firstpress = false;
     BT_PLAY_PUASE();
     mute();
-    }
+  }
 
-  if (!Muted && lastState == LOW && !btMode && state == HIGH && (!modeBtnHeld && (millis()-modeBtnPressTime >1430))){
+  if (!Muted && lastState == LOW && !btMode && state == HIGH && (!modeBtnHeld && (millis() - modeBtnPressTime > 1430))) {
     VOLUME = !VOLUME;
-    }
-    if (lastState == LOW && !Muted && !btMode && state == LOW && (!modeBtnHeld && (millis()-modeBtnPressTime >1450))){
-      if(!VOLUME){
-    lcd.setCursor(0, 0);
-    lcd.print("Select Volume   ");
+  }
+  if (lastState == LOW && !Muted && !btMode && state == LOW && (!modeBtnHeld && (millis() - modeBtnPressTime > 1450))) {
+    if (!VOLUME) {
+      lcd.setCursor(0, 0);
+      lcd.print("Select Volume   ");
     } else {
-    lcd.setCursor(0, 0);
-    lcd.print("Set at");
-    lcd.setCursor(7, 0);
-    lcd.print(volStep);
-    lcd.print("         ");
+      lcd.setCursor(0, 0);
+      lcd.print("Set at");
+      lcd.setCursor(7, 0);
+      lcd.print(volStep);
+      lcd.print("         ");
     }
-    }
-    // detect exit from volume mode
-if (!VOLUME && lastVolumeState == true) {
+  }
+  // detect exit from volume mode
+  if (!VOLUME && lastVolumeState == true) {
     volumeExitTime = millis();
     volumeExitPending = true;
-}
-lastVolumeState = VOLUME;
+  }
+  lastVolumeState = VOLUME;
   lastState = state;
 }
 
-void PREV () {
+void PREV() {
   digitalWrite(BT_VOLUP, HIGH);
   delay(350);
   digitalWrite(BT_VOLUP, LOW);
 }
 
-void NEXT(){
+void NEXT() {
   digitalWrite(BT_VOLDOWN, HIGH);
   delay(350);
   digitalWrite(BT_VOLDOWN, LOW);
 }
 
 void mute() {
-   if(Muted == false && radioIsOn){
-  radio.setVolume(0);
+  if (Muted == false && radioIsOn) {
+    radio.setVolume(0);
     Muted = true;
   } else if (radioIsOn) {
-  int radioVol = ((volStep * 63) / 63);
-  radio.setVolume(radioVol);
+    int radioVol = ((volStep * 63) / 63);
+    radio.setVolume(radioVol);
     Muted = false;
   }
-  if (Muted && !btMode){
+  if (Muted && !btMode) {
     lcd.setCursor(0, 1);
     lcd.print("    ");
     lcd.setCursor(0, 1);
     lcd.print("MUTE");
-    } else if (!btMode) {
+  } else if (!btMode) {
     int voldisp = (volStep * 1);
     lcd.setCursor(0, 1);
     lcd.print("V:");
     lcd.print(voldisp);
-    }
+  }
 }
 
 void simulatePowerButtonPress() {   // Enable pin drive
@@ -487,10 +487,10 @@ void simulatePowerButtonPress() {   // Enable pin drive
 }
 
 void BT_PLAY_PUASE() {
-  if (btMode){
+  if (btMode) {
     digitalWrite(PIN_PWR_SIM, HIGH);
-    delay(400);                      // Hold for 3.5 seconds
-    digitalWrite(PIN_PWR_SIM, LOW); 
+    delay(400);  // Hold for 3.5 seconds
+    digitalWrite(PIN_PWR_SIM, LOW);
   }
 }
 
@@ -501,73 +501,73 @@ void updateRadio() {
 
 void handlevolumeControl(int diff) {
   if (!btMode) {
-  volStep += diff * volchange ;
-  volStep = constrain(volStep,volmin,volmax);
-  volumedisplay();
-  updateRadio();
-}
+    volStep += diff * volchange;
+    volStep = constrain(volStep, volmin, volmax);
+    volumedisplay();
+    updateRadio();
+  }
 }
 
 void handleFrequencyControl(int diff) {
-    if (!btMode){
-      switch (currentMode) {
-        case MODE_FM:
-          freqFM += diff * FM_STEP;
-          if (freqFM < FM_MIN ){
-            freqFM = FM_MAX;
-          }
-          if (freqFM > FM_MAX ){
-            freqFM = FM_MIN;
-          }
-          freqFM = constrain(freqFM, FM_MIN, FM_MAX);
-          radio.setFrequency((uint16_t)(freqFM * 100));
-          updateradioDisplay();
-          break;
-        case MODE_AM:
-          freqAM += diff * AM_STEP;
-          if (freqAM < AM_MIN ){
-            freqAM = AM_MAX;
-          }
-          if (freqAM > AM_MAX ){
-            freqAM = AM_MIN;
-          }
-          freqAM = constrain(freqAM, AM_MIN, AM_MAX);
-          radio.setFrequency(freqAM);
-          updateradioDisplay();
-          break;
-        case MODE_SW:
-          freqSW += diff * SW_STEP;
-          if (freqSW < SW_MIN ){
-            freqSW = SW_MAX;
-          }
-          if (freqSW > SW_MAX ){
-            freqSW = SW_MIN;
-          }
-          freqSW = constrain(freqSW, SW_MIN, SW_MAX);
-          radio.setFrequency(freqSW);
-          updateradioDisplay();
-          break;
-        case MODE_LW:
-          freqLW += diff * LW_STEP;
-          if (freqLW < LW_MIN ){
-            freqLW = LW_MAX;
-          }
-          if (freqLW > LW_MAX ){
-            freqLW = LW_MIN;
-          }
-          freqLW = constrain(freqLW, LW_MIN, LW_MAX);
-          radio.setFrequency(freqLW);
-          updateradioDisplay();
-          break;
-        default:
-          break;
-      }
+  if (!btMode) {
+    switch (currentMode) {
+      case MODE_FM:
+        freqFM += diff * FM_STEP;
+        if (freqFM < FM_MIN) {
+          freqFM = FM_MAX;
+        }
+        if (freqFM > FM_MAX) {
+          freqFM = FM_MIN;
+        }
+        freqFM = constrain(freqFM, FM_MIN, FM_MAX);
+        radio.setFrequency((uint16_t)(freqFM * 100));
+        updateradioDisplay();
+        break;
+      case MODE_AM:
+        freqAM += diff * AM_STEP;
+        if (freqAM < AM_MIN) {
+          freqAM = AM_MAX;
+        }
+        if (freqAM > AM_MAX) {
+          freqAM = AM_MIN;
+        }
+        freqAM = constrain(freqAM, AM_MIN, AM_MAX);
+        radio.setFrequency(freqAM);
+        updateradioDisplay();
+        break;
+      case MODE_SW:
+        freqSW += diff * SW_STEP;
+        if (freqSW < SW_MIN) {
+          freqSW = SW_MAX;
+        }
+        if (freqSW > SW_MAX) {
+          freqSW = SW_MIN;
+        }
+        freqSW = constrain(freqSW, SW_MIN, SW_MAX);
+        radio.setFrequency(freqSW);
+        updateradioDisplay();
+        break;
+      case MODE_LW:
+        freqLW += diff * LW_STEP;
+        if (freqLW < LW_MIN) {
+          freqLW = LW_MAX;
+        }
+        if (freqLW > LW_MAX) {
+          freqLW = LW_MIN;
+        }
+        freqLW = constrain(freqLW, LW_MIN, LW_MAX);
+        radio.setFrequency(freqLW);
+        updateradioDisplay();
+        break;
+      default:
+        break;
+    }
   }
-  }
+}
 
-  
-  void updateradioDisplay() {
-    if(!btMode){
+
+void updateradioDisplay() {
+  if (!btMode) {
     lcd.setCursor(0, 0);
     switch (currentMode) {
       case MODE_FM:
@@ -606,20 +606,20 @@ void handleFrequencyControl(int diff) {
         break;
     }
   }
-  }
+}
 
-  void updateRDS() {
-    if (wait - millis() >= 2000 && wait - millis() <= 2030 && !VOLUME){
-     updateradioDisplay();
+void updateRDS() {
+  if (wait - millis() >= 2000 && wait - millis() <= 2030 && !VOLUME) {
+    updateradioDisplay();
   }
-    if (radioIsOn) {
+  if (radioIsOn) {
 
     radio.rdsBeginQuery();  // same as getRdsStatus()
 
     const char *rt = radio.getRdsText2A();  // returns pointer to radio-text buffer
 
-    // If text pointer is null or empty, clear space
-    if ( currentMode != MODE_FM || rt == NULL || rt[0] == '\0') {
+    // If text pointer is null or empty, scroll char
+    if (currentMode != MODE_FM || rt == NULL || rt[0] == '\0') {
       rt = "            Still Your Boy CHUKWUEBUKA (GOD_IS_WITH_US), Have A Blessful Day And Keep Listening To The RADIO While It Last.";
     }
 
@@ -647,33 +647,35 @@ void handleFrequencyControl(int diff) {
     // 5. Print it
     lcd.setCursor(5, 1);
     lcd.print(disp);
-  }}
+  }
+}
 
-  void displayBTMode() { 
+void displayBTMode() {
   if (btMode) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("BLUETOOTH MODE  ");
     lcd.setCursor(0, 1);
     lcd.print("CONNECT=> TM-078");
-  }}
+  }
+}
 
-  void volumedisplay() {
-  if (radioIsOn && !btMode && !Muted){
+void volumedisplay() {
+  if (radioIsOn && !btMode && !Muted) {
     int voldisp = (volStep * 1);
     lcd.setCursor(0, 1);
-     lcd.print("V:");
-    if (voldisp<10){
-    lcd.print(0);
+    lcd.print("V:");
+    if (voldisp < 10) {
+      lcd.print(0);
     }
     lcd.print(voldisp);
-    } else if (radioIsOn && !btMode){
+  } else if (radioIsOn && !btMode) {
     lcd.setCursor(0, 1);
     lcd.print("    ");
     lcd.setCursor(0, 1);
     lcd.print("MUTE");
-    }
   }
+}
 
 void updateEncoder() {
   int MSB = digitalRead(PIN_ENC_FREQ_A);
@@ -692,7 +694,3 @@ void updateEncoder() {
 
   lastEncoded = encoded;
 }
-
-
-
-
